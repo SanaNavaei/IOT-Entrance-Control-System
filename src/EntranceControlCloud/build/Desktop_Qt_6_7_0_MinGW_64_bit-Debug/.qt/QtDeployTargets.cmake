02d6cf3e447a4de541ffb6f526ecea5ebab4c50f
Hadi <m.h.babalu@gmail.com>
2024-04-23 03:25:23 +0330

    feat: initial entrance control cloud #1

---
+++ src/EntranceControlCloud/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_EntranceControlCloud_FILE G:/Uni/term_8/CPS/Projects/CA1_IOT/src/EntranceControlCloud/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/EntranceControlCloud.exe)
+set(__QT_DEPLOY_TARGET_EntranceControlCloud_RUNTIME_DLLS F:/Qt/6.7.0/mingw_64/bin/Qt6HttpServer.dll;F:/Qt/6.7.0/mingw_64/bin/Qt6WebSockets.dll;F:/Qt/6.7.0/mingw_64/bin/Qt6Network.dll;F:/Qt/6.7.0/mingw_64/bin/Qt6Core.dll)
